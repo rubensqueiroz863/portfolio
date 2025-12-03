fc15ef8937bebe1dc9d2a900a5021ca4c52e3c5d
rubensqueiroz863 <rubensqueiroz863@exemplo.com>
2025-12-03 11:53:11 -0300

    Componente NavBar

---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,7 @@
+export default function NavBar() {
+  return (
+    <div className="w-full h-20 bg-neutral-900">
+      
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import NavBar from "./components/NavBar";
 import { ScienceGothic } from "./layout";
 
 export default function Home() {
   return (
     <div className={`w-full h-screen bg-neutral-900 ${ScienceGothic.className}`}>
-      
+      <NavBar />
+      <div className="w-full h-[0.1] bg-neutral-700"></div>
     </div>
   );
 }
