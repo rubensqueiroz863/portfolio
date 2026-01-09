48f0fcc5ed5804dc9371cfeb4ef4aca8ac5b6451
rubensqueiroz863 <rubensqueiroz863@exemplo.com>
2026-01-09 14:39:38 -0300

    feat(warning): Adição de aviso de 'Under construction'

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -48,6 +48,14 @@ export default function ProjectsPage() {
         >
           {traslation("description")}
         </motion.p>
+        <motion.h1
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+          className={`text-[#161b28] my-20 dark:text-[#5c4f2e] md:text-4xl text-[28px] ${ScienceGothic.className}`}
+        >
+          ...Under construction
+        </motion.h1>
       </div>
     </div>
   );
